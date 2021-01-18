e7e67ed10b3c4dc49e0f247bfcf1e2fe89864c7c
rsv_k <rsv.kpast@gmail.com>
2021-01-18 16:03:33 +0200

    add function setActive class and add onclick on every option in popup.ts

--- popup.ts
+++ popup.ts
@@ -1,1 +1,27 @@
-console.log('popup.js');
+const options = document.getElementsByClassName('option');
+
+function setActiveClass(text: string) {
+	for (let i = 0; i < options.length; i++) {
+		const o = options[i] as HTMLElement;
+		o.classList.remove('active');
+
+		if (o.textContent === text) {
+			o.classList.add('active');
+		}
+	}
+}
+
+chrome.storage.local.get(['timer'], function (result) {
+	if (result.timer) {
+		setActiveClass(result.timer);
+	}
+
+	for (let i = 0; i < options.length; i++) {
+		const o = options[i] as HTMLElement;
+
+		o.onclick = () =>
+			chrome.storage.local.set({ timer: o.textContent }, () => {
+				setActiveClass(o.textContent as string);
+			});
+	}
+});
