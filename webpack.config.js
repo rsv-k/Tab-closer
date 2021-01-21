b981e1a86372f8496ede97c8082ecc6ad2123133
rsv_k <rsv.kpast@gmail.com>
2021-01-21 11:48:09 +0200

    add basic webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+	entry: {
+		background: './src/background/background.ts',
+		popup: './src/popup/popup.ts',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	// devtool: 'inline-source-map',
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+};
